2f5234f51d2ebb36a09ee539a5131076ed026969
Robin <robin@robintindale.com>
2018-04-09 02:47:47 +0100

    rewrite communicating plugin->native by injecting an objective C class into javascript

--- client.js
+++ client.js
@@ -2,16 +2,5 @@ module.exports = function(actionName) {
   if (!actionName) {
     throw new Error('missing action name')
   }
-  var args = [].slice.call(arguments).slice(1)
-  var previousHash = window.location.hash.split('?')[1]
-    ? window.location.hash.split('?')[0]
-    : window.location.hash
-  window.location.hash =
-    previousHash +
-    '?pluginAction=' +
-    encodeURIComponent(actionName) +
-    '&actionId=' +
-    Date.now() +
-    '&pluginArgs=' +
-    encodeURIComponent(JSON.stringify(args))
+  window.sketchBridge.callNative(JSON.stringify([].slice.call(arguments)))
 }
