3d111641447f18fe5f1a85c204faa63629767871
nichenqin <nichenqin@gmail.com>
2018-04-12 17:02:27 +0800

    :bug: safer check

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
-var CONSTANTS = require('./lib/constants')
+var { JS_BRIDGE } = require('./lib/constants')
 
 module.exports = function(actionName) {
   if (!actionName) {
     throw new Error('missing action name')
   }
-  window[CONSTANTS.JS_BRIDGE].callNative(
-    JSON.stringify([].slice.call(arguments))
-  )
+
+  if(window[JS_BRIDGE] && typeof window[JS_BRIDGE].callNative === 'function') {
+    window[JS_BRIDGE].callNative(
+      JSON.stringify([].slice.call(arguments))
+    )
+  }
 }
